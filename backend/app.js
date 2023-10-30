b307be3c973a4d2708082be8d38331b131ee2e05
krishsuthar1504 <131298127+krishsuthar1504@users.noreply.github.com>
2023-10-30 21:10:56 +0530

    database_connected

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
 const exp = require("express");
 const app = exp();
-app.arguments(express.json());
-
 const product = require("./routes/productRoute");
 
-app.arguments("/api/v1",product)
+
+app.use(exp.json());
+app.use("/api/v1",product)
+
+
+
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const app = require("./app");
 const dotenv = require("dotenv");
-
+const connectDatabase = require("./database/database")
 
 //config
 dotenv.config({path:"backend/config/config.env"});
 
+// connecting the database
+connectDatabase();
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server Started Running at http://localhost:${process.env.PORT}`);
