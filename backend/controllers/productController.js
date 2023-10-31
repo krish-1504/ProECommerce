029352692f4b075214c5d25faecd2ff4b25f8206
krishsuthar1504 <131298127+krishsuthar1504@users.noreply.github.com>
2023-10-31 21:35:24 +0530

    gettingALLProducts

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,7 @@
 const Product = require("../models/productModel");
 
 
-//create Product
+//Create Product
 exports.createProduct = async (req,res,next)=>{
     const product = await Product.create(req.body);
     res.status(201).json({
@@ -10,6 +10,12 @@ exports.createProduct = async (req,res,next)=>{
     })
 }
 
-exports.getAllProducts = (req,res) =>{
-    res.status(200).json({message:"Working Perfectly Fine Kriiishh!!"});
+
+//Get All Product
+exports.getAllProducts = async (req,res) =>{
+    const products = await Product.find();
+    res.status(200).json({
+        success:true,
+        products
+    });
 }
