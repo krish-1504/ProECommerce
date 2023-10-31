b9c63210a5f40887f6de33cfdba0fd608627d9f8
krishsuthar1504 <131298127+krishsuthar1504@users.noreply.github.com>
2023-10-31 21:05:13 +0530

    Update productModel.js

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -25,7 +25,55 @@ const productSchema = mongoose.Schema({
             public_id:{
                 type:String,
                 required:true,
-            }
-        }
-    ]
+            },
+            url:{
+                type:String,
+                required:true,
+            },
+        },
+    ],
+    category:{
+        type:String,
+        required:[true,"Please Enter Product Category"],
+    },
+    Stock:{
+        type:Number,
+        required:[true,"Please Enter Product Stock"],
+        maxLength:[4,"Stock cannot exceed 4 Characters"],
+        default:1,
+    },
+    numOfReviews: {
+        type: Number,
+        default: 0,
+      },
+    reviews: [
+    {
+        user: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+        required: true,
+        },
+        name: {
+        type: String,
+        required: true,
+        },
+        rating: {
+        type: Number,
+        required: true,
+        },
+        comment: {
+        type: String,
+        required: true,
+        },
+    },
+    ],
+    user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+    },
+    createdAt: {
+    type: Date,
+    default: Date.now,
+    },
 })
