78c545612f1aa6adabbc940bf94a6b5058832b97
krishsuthar1504 <131298127+krishsuthar1504@users.noreply.github.com>
2023-11-05 16:58:39 +0530

    SearchAndFilterInAPI

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/productModel");
 const ErrorHandler = require("../utils/errorHandler");
-const catchAsyncErrors = require("../middleware/catchAsyncErrors")
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ApiFeatures = require("../utils/apifeatures");
 
 //Create Product -Admin
 exports.createProduct = catchAsyncErrors(async (req,res,next)=>{
@@ -14,7 +15,8 @@ exports.createProduct = catchAsyncErrors(async (req,res,next)=>{
 
 //Get All Product -- Admin
 exports.getAllProducts = catchAsyncErrors(async (req,res) =>{
-    const products = await Product.find();
+    const apiFeature = new ApiFeatures(Product.find(),req.query).search().filter();
+    const products = await apiFeature.query;
     res.status(200).json({
         success:true,
         products
