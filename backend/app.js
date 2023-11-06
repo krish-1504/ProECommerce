d8e13f8e2103d73ce971aa883d15d323bc8d4bd8
krishsuthar1504 <131298127+krishsuthar1504@users.noreply.github.com>
2023-11-06 18:31:14 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const app = exp();
 const product = require("./routes/productRoute");
 const express = require("express");
 const errorMiddleware = require("./middleware/error")
+const user = require("./routes/userRoute")
 // Use body-parser middleware
 
 const bodyParser = require('body-parser');
@@ -11,6 +12,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(exp.json());
 app.use("/api/v1",product)
+app.use("/api/v1",user)
 
 // Middleware For Errors
 app.use(errorMiddleware);
