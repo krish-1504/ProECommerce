b47e73dc6eabaeb2a9ccf59e9ea919c8a08925d5
krishsuthar1504 <131298127+krishsuthar1504@users.noreply.github.com>
2023-11-27 14:55:02 +0530

    GetAllProducts

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -2,6 +2,13 @@ import Product from "../models/product";
 
 export const newProduct = async (req,res,next)=>{
     const product = await Product.create(req.body);
+    res.status(201).json({
+        product,
+    });
+}
+
+export const getProducts = async (req,res,next)=>{
+    const product = await Product.find();
     res.status(200).json({
         product,
     });
--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,10 +1,12 @@
 import { createRouter } from 'next-connect';
 import dbConnect from '@/backend/config/dbConnect';
-import { newProduct } from '@/backend/controllers/productControllers';
+import { getProducts, newProduct } from '@/backend/controllers/productControllers';
 
 dbConnect();
 
 const router = createRouter()
+router.get(getProducts);
 router.post(newProduct);
+
 
 export default router.handler();
