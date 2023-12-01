57064fc3686f9d863e28747985b54760dd553ba5
krishsuthar1504 <131298127+krishsuthar1504@users.noreply.github.com>
2023-12-01 16:00:33 +0530

    User Model

--- app/GlobalProvider.jsx
+++ app/GlobalProvider.jsx
@@ -2,19 +2,14 @@
 
 import { AuthProvider } from "@/context/AuthContext";
 import { CartProvider } from "@/context/CartContext";
-import { SessionProvider } from "next-auth/react";
-
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 export function GlobalProvider({ children }) {
   return (
     <>
       <ToastContainer position="bottom-right" />
       <AuthProvider>
-        <CartProvider>
-          <SessionProvider>{children}</SessionProvider>
-        </CartProvider>
+        <CartProvider>{children}</CartProvider>
       </AuthProvider>
     </>
   );
