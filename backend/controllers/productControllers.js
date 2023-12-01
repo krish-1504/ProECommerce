c28737188fccb5a53a9bbaa20b0ec2ed478e7367
krishsuthar1504 <131298127+krishsuthar1504@users.noreply.github.com>
2023-12-01 15:49:54 +0530

    Filters

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,32 +1,46 @@
 import Product from "../models/product";
 import APIFilters from "../utils/APIFilters";
-export const newProduct = async (req,res,next)=>{
-    const product = await Product.create(req.body);
-    res.status(201).json({
-        product,
+
+export const newProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+  res.status(201).json({
+    product,
+  });
+};
+
+export const getProducts = async (req, res, next) => {
+  const resPerPage = 2;
+  const productsCount = await Product.countDocuments();
+
+  const apiFilters = new APIFilters(Product.find(), req.query)
+    .search()
+    .filter();
+
+  let products = await apiFilters.query;
+  const filteredProductsCount = products.length;
+
+  apiFilters.pagination(resPerPage);
+
+  products = await apiFilters.query.clone();
+
+  res.status(200).json({
+    productsCount,
+    resPerPage,
+    filteredProductsCount,
+    products,
+  });
+};
+
+export const getProduct = async (req, res, next) => {
+  const product = await Product.findById(req.query.id);
+
+  if (!product) {
+    res.status(404).json({
+      error: "Product not found.",
     });
-}
+  }
 
-export const getProducts = async (req,res,next)=>{
-
-
-    const apiFilters = new APIFilters(Product.find(),req.query).search().filter()
-    const product = await apiFilters.query;
-
-
-    res.status(200).json({
-        product,
-    });
-}
-
-export const getProduct = async (req,res,next)=>{
-    const product = await Product.findById(req.query.id);
-    if(!product){
-        res.status(404).json({
-            error:'Product Not Found',
-        });
-    }
-    res.status(200).json({
-        product,
-    });
-}
+  res.status(200).json({
+    product,
+  });
+};
--- components/products/ListProducts.jsx
+++ components/products/ListProducts.jsx
@@ -1,28 +1,32 @@
-"use client"
-import React from 'react'
-import Filters from '../layout/Filters';
+"use client";
 
-import Productitem from './Productitem';
+import React from "react";
+import CustomPagination from "../layout/CustomPagination";
+import Filters from "../layout/Filters";
 
-const ListProducts = ({data}) => {
-    console.log(data);
+import ProductItem from "./ProductItem";
+
+const ListProducts = ({ data }) => {
   return (
     <section className="py-12">
-        <div className="container max-w-screen-xl mx-auto px-4">
-            <div className="flex flex-col md:flex-row -mx-4">
-                <Filters />
+      <div className="container max-w-screen-xl mx-auto px-4">
+        <div className="flex flex-col md:flex-row -mx-4">
+          <Filters />
 
-                <main className="md:w-2/3 lg:w-3/4 px-3">
-                    {data?.product?.map((product)=>(
-                        <Productitem key={product?._id} product={product}></Productitem>
-                    ))}
-                </main>
-            </div>
+          <main className="md:w-2/3 lg:w-3/4 px-3">
+            {data?.products?.map((product) => (
+              <ProductItem key={product?._id} product={product} />
+            ))}
+
+            <CustomPagination
+              resPerPage={data?.resPerPage}
+              productsCount={data?.filteredProductsCount}
+            />
+          </main>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default ListProducts
-
-
+export default ListProducts;
